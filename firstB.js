4b9557fd833ee1daeeaeffc1cdb44248f356672f
mmshareef <muhammadmuadassirshareef@gmail.com>
2021-03-21 18:17:41 +0530

    1b solved

--- firstB.js
+++ firstB.js
@@ -10,3 +10,10 @@ I found tea@ching m%o@re interesting tha@n any other %jo@bs.
 There is nothing as more rewarding as educating and empowering people. 
 I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
 }*/
+
+const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+function cleanText(sentence) {
+   return newsentence =sentence.replace(/[^\w\s]/gi, '');
+
+}
+console.log(cleanText(sentence));
