7ea0f899b5d7b31cec3b28f8c5833f7997f81a8d
mmshareef <muhammadmuadassirshareef@gmail.com>
2021-03-21 18:36:08 +0530

    1c solved

--- firstC.js
+++ firstC.js
@@ -9,3 +9,20 @@ const sentence =  `I am a teacher and I love teaching.
 console.log(countWords(sentence));
 31
 }*/
+
+
+function countWords(sentence) {
+    let res = [];
+    res1 =sentence.split(' ');
+    for(let i=0;i<res1.length;i++) {
+        if(res1[i].length > 1) {
+            res.push(res1[i]);
+        }
+    }
+    return res.length;
+}
+const sentence = `I am a teacher and I love teaching. 
+There is nothing as more rewarding as educating and empowering people.
+ I found teaching more interesting than any other jobs.
+  Does this motivate you to be a teacher?`;
+console.log(countWords(sentence));
