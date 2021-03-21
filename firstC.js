048095d9d2f53de765a700380c859c7076dada80
mmshareef <muhammadmuadassirshareef@gmail.com>
2021-03-21 18:27:08 +0530

    question 1c added

---
+++ firstC.js
@@ -0,0 +1,11 @@
+/*{1.c. After cleaning the text in the sentence from question number b you will get the
+    following text. Count the number of words in this text. Don't include words with only 
+    one letter.
+
+```js
+const sentence =  `I am a teacher and I love teaching.
+ There is nothing as more rewarding as educating and empowering people.
+  I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
+console.log(countWords(sentence));
+31
+}*/
